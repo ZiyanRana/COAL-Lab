0e775cecf2e3be4052a4253c74b10b63a0bce2fe
ZiyanRana <mziyanrana@gmail.com>
2026-03-14 17:22:48 +0500

    Added Lab 5 work

---
+++ Lab 5/Task8/test.asm
@@ -0,0 +1,32 @@
+INCLUDE Irvine32.inc
+
+.data
+    prompt BYTE "Enter N: ", 0
+    num    DWORD ?
+    space  BYTE " ", 0
+
+.code
+main PROC
+
+    mov EDX, OFFSET prompt
+    call WriteString
+    call ReadInt
+    mov num, EAX
+
+    mov EAX, 2
+    mov ECX, num
+
+EvenLoop:
+    cmp EAX, ECX
+    jg Done
+    call WriteInt
+    mov EDX, OFFSET space
+    call WriteString
+    add EAX, 2
+    jmp EvenLoop
+
+Done:
+    call Crlf
+    exit
+main ENDP
+END main
