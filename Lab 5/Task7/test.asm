26ea26a451baf957173a2f6f09c547b5eaffd23f
ZiyanRana <mziyanrana@gmail.com>
2026-03-14 17:19:38 +0500

    Added Lab 5 work

--- Lab 5/Task7/test.asm
+++ Lab 5/Task7/test.asm
@@ -1,14 +1,47 @@
-.386 
-.model flat,stdcall 
-.stack 4096 
-ExitProcess proto,dwExitCode:dword 
+INCLUDE Irvine32.inc
 
-.code 
-main proc 
-	mov eax,5	; Moving 5 to EAX Register
-	add eax,6	; Adding 6 to the register EAX and store the result in the EAX itself 
- 
-	invoke ExitProcess,0 
+.data
+    prompt      BYTE "Enter string: ", 0
+    reversedMsg BYTE "Reversed string: ", 0
+    inputBuffer BYTE 100 DUP(0)
+    bufSize     DWORD SIZEOF inputBuffer
 
-main endp 
-end main 
+.code
+main PROC
+
+    mov  EDX, OFFSET prompt
+    call WriteString
+
+    mov  EDX, OFFSET inputBuffer
+    mov  ECX, bufSize
+    call ReadString
+
+    dec  EAX
+
+    mov  ESI, OFFSET inputBuffer
+    mov  EDI, OFFSET inputBuffer
+    add  EDI, EAX
+
+    mov  ECX, EAX
+    shr  ECX, 1
+
+SwapLoop:
+    mov  AL, [ESI]
+    mov  BL, [EDI]
+    mov  [ESI], BL
+    mov  [EDI], AL
+    inc  ESI
+    dec  EDI
+    loop SwapLoop
+
+    call Crlf
+    mov  EDX, OFFSET reversedMsg
+    call WriteString
+    
+    mov  EDX, OFFSET inputBuffer
+    call WriteString
+    call Crlf
+
+    exit
+main ENDP
+END main
