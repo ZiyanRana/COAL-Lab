8ed3b90c9be170ec2755436ea46fa7f09b9b9e6e
ZiyanRana <mziyanrana@gmail.com>
2026-03-14 15:16:13 +0500

    Uploaded Lab 3 work

---
+++ Lab 3/Task3/test.asm
@@ -0,0 +1,30 @@
+INCLUDE Irvine32.inc
+
+.data
+msg1 BYTE "Enter first number: ",0
+msg2 BYTE "Enter second number: ",0
+resultMsg BYTE "Product = ",0
+
+.code
+main PROC
+
+    mov edx, OFFSET msg1
+    call WriteString
+    call ReadInt
+    mov ebx, eax
+
+    mov edx, OFFSET msg2
+    call WriteString
+    call ReadInt
+
+    imul eax, ebx
+
+    mov edx, OFFSET resultMsg
+    call WriteString
+    call WriteInt
+    call Crlf
+
+    exit
+main ENDP
+
+END main
